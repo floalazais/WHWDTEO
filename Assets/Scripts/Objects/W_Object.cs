c8e33df098790219c08e10d85af49847221d8fbf
Valefor <memeH@hotmail.fr>
2020-01-15 15:57:47 +0100

    [UPDATE] Exploration structure

---
+++ Assets/Scripts/Objects/W_Object.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class W_Object : MonoBehaviour
+{
+    [SerializeField]protected Material _memoryObjectMaterial = null;
+    [SerializeField]protected Material _presentObjectMaterial = null;
+
+    [SerializeField] protected MeshFilter _memoryObjectFilter = null;
+    [SerializeField] protected MeshFilter _presentObjectFilter = null;
+
+    [SerializeField] protected MeshRenderer _meshRenderer = null;
+    protected MeshFilter _meshFilter = null;
+
+    private void Start()
+    {
+        _meshRenderer = GetComponent<MeshRenderer>();
+        _meshFilter = GetComponent<MeshFilter>();
+    }
+
+    public virtual void SetModePresent()
+    {
+        if (_presentObjectMaterial == null && _presentObjectFilter == null)
+        {
+            _meshRenderer = null;
+            return;
+        }
+
+        _meshFilter = _presentObjectFilter;
+        _meshRenderer.material = _presentObjectMaterial; 
+    }
+
+    public virtual void SetModeMemory()
+    {
+        if (_memoryObjectMaterial == null && _memoryObjectFilter == null)
+        {
+            _meshRenderer = null;
+            return;
+        }
+
+        _meshFilter = _memoryObjectFilter;
+        _meshRenderer.material = _memoryObjectMaterial;
+    }
+}
