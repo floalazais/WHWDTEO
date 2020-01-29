baa6c8f986022f110a3d8463759b919a497414a4
Theo V <tcn.verpillat@yahoo.fr>
2020-01-29 11:47:53 +0100

    [ADD]Plushies Fade In

---
+++ Assets/_Personal/Theo/Cinemachine_Scene/Test Scripts/PlushFadeIn.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlushFadeIn : MonoBehaviour
+{
+    [SerializeField] private Material transparentMaterial;
+    private Material[] _childrenMaterials;
+    private Renderer[] _childrenRenderers;
+
+    public void StartFade(float seconds)
+    {
+        Debug.Log("Start Fade");
+        _childrenRenderers = GetComponentsInChildren<Renderer>();
+        _childrenMaterials = new Material[_childrenRenderers.Length];
+        for (int i = 0; i < _childrenRenderers.Length; i++)
+        {
+            _childrenMaterials[i] = _childrenRenderers[i].material;
+        }
+        foreach (Renderer rend in _childrenRenderers) rend.material = transparentMaterial;
+
+        StartCoroutine(FadeIn(seconds));
+    }
+
+
+    IEnumerator FadeIn (float fadeTime)
+    {
+        float elapsedTime = 0;
+        while(elapsedTime < fadeTime)
+        {
+            elapsedTime += Time.deltaTime;
+            //float lerp = Mathf.PingPong(Time.time, fadeTime) / fadeTime;
+            for (int i = 0; i < _childrenRenderers.Length; i++)
+            {
+                _childrenRenderers[i].material.Lerp(transparentMaterial, _childrenMaterials[i], elapsedTime);
+            }
+            yield return new WaitForSeconds(Time.deltaTime);
+            Debug.Log("Time: " + elapsedTime);
+        }
+    }
+}
