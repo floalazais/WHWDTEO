2c76d2a382d49cc1468168a069db530075e0fa88
Valefor <memeH@hotmail.fr>
2020-02-14 14:49:38 +0100

    [ADD] FzdeOut fix controllers

--- Assets/Scripts/UI/ChoiceElement.cs
+++ Assets/Scripts/UI/ChoiceElement.cs
@@ -6,16 +6,25 @@ using UnityEngine.UI;
 public class ChoiceElement : MonoBehaviour
 {
     Text _choiceText = null;
+    Animator _choiceAnimator = null;
 
     // Start is called before the first frame update
     void Start()
     {
         _choiceText = GetComponentInChildren<Text>();
+        _choiceAnimator = GetComponent<Animator>();
     }
 
     public void EnableChoiceElement(string pText)
     {
-        if(_choiceText == null) _choiceText = GetComponentInChildren<Text>();
+        _choiceAnimator.SetBool(Utils_Variables.IS_FADING, false);
+        if (_choiceText == null) _choiceText = GetComponentInChildren<Text>();
         _choiceText.text = pText;
+    }
+
+    public void FadeOut()
+    {
+        _choiceAnimator.SetBool(Utils_Variables.IS_FADING, true);
+        //_choiceAnimator.Play("FadeOut");
     }
 }
